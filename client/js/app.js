2a96b0330dd35ef1f0e96720b60642ae9ac29cd5
Christian Rudh <christian@rudh.se>
2015-06-13 21:31:31 +0200

    Fixed root path for index file.

--- client/js/app.js
+++ client/js/app.js
@@ -4,7 +4,7 @@ require("./core/flux/flux");
 
 const React = require("react");
 const Router = require("react-router");
-const { Route, DefaultRoute, NotFoundRoute, RouteHandler,  Link } = Router;
+const {Route, DefaultRoute, NotFoundRoute, RouteHandler,  Link} = Router;
 const NotFoundView = require("./core/notfoundview");
 const IndexView = require("./indexview");
 const TasksView = require("./tasks/tasksview");
--- server/server.js
+++ server/server.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(express.static("public"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 tasks.init(app);
 
 app.get('*', (req, res) => {
-  res.sendFile("public/index.html");
+  res.sendFile("public/index.html", {"root": __dirname + "../public/"});
 });
 
 app.listen(3000);
